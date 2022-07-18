27cf919b124f8fb513fe9b8e80645dbd55685cf1
林志达 <zhidal@qq.com>
2022-07-18 11:51:03 +0800

    🎉 init: 初始化项目

---
+++ .changes/initial-release.md
@@ -0,0 +1,6 @@
+---
+"tauri-plugin-serialport": "minor"
+"tauri-plugin-serialport-api": "minor"
+---
+
+Initial release.
---
+++ examples/svelte-app/src-tauri/src/main.rs
@@ -0,0 +1,17 @@
+#![cfg_attr(
+  all(not(debug_assertions), target_os = "windows"),
+  windows_subsystem = "windows"
+)]
+
+fn main() {
+  let context = tauri::generate_context!();
+  tauri::Builder::default()
+    .menu(if cfg!(target_os = "macos") {
+      tauri::Menu::os_default(&context.package_info().name)
+    } else {
+      tauri::Menu::default()
+    })
+    .plugin(tauri_plugin_serialport::init())
+    .run(context)
+    .expect("failed to run app");
+}
