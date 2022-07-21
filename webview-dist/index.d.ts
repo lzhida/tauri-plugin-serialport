48f9e005d2040d93eca01aab0d97b3a1728611db
林志达 <zhidal@qq.com>
2022-07-21 17:37:26 +0800

    🦄 refactor: 添加声明

--- webview-dist/index.d.ts
+++ webview-dist/index.d.ts
@@ -1,1 +1,35 @@
+export interface OpenOption {
+  path: string;
+  baudRate: number;
+}
+
+export interface WriteOption {
+  path: string;
+  value: string;
+}
+
+export interface ReadOptions {
+  path: string;
+  readEvent?: string;
+}
+
+interface InvokeResult {
+  code: number,
+  message: string,
+  [key: string]:any
+}
+
 export declare function execute(): Promise<void>
+export declare function open(options: OpenOption): Promise<InvokeResult>
+
+export declare function close(path: string): Promise<InvokeResult>
+
+export declare function write(options: WriteOption): Promise<InvokeResult>
+
+export declare function read(options: ReadOptions): Promise<InvokeResult> 
+
+export declare function listen<T>(
+  event: string,
+  handler: (...args: any[]) => void,
+): Promise<InvokeResult>
+
